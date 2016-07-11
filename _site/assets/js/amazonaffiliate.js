4d8f480cf06e6668b18caa7b407f0880c9d978d5
Jake McClure <McClure@Jakes-MacBook-Air.local>
2016-07-11 04:22:16 -0400

    add amazon links

--- _site/assets/js/amazonaffiliate.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-    amzn_assoc_ad_type = "link_enhancement_widget";
-    amzn_assoc_tracking_id = "mcclure-20";
-    amzn_assoc_linkid = "b7b803cee20bb728b9c2348061fa27a8";
-    amzn_assoc_placement = "";
-    amzn_assoc_marketplace = "amazon";
-    amzn_assoc_region = "US";
-</script>
-<script src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&MarketPlace=US"></script>
